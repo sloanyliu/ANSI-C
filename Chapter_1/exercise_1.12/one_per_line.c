6000c0f82af72080abbf94a0648ea5caf9913c1a
Sloan <sloanyianliu@gmail.com>
2021-03-10 21:24:53 -0800

    minor changes

--- Chapter_1/exercise_1.12/one_per_line.c
+++ Chapter_1/exercise_1.12/one_per_line.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
-/*
- * 
- *
- *
+/* prints out the words of a message one per line
+ * multiple spaces are counted as one space
  */
 
 int
 main (void) 
 {
-	int c, flag;
-	c = flag = 0;
+  int c, flag;
+  c = flag = 0;
 
-	puts("Enter some words separated by spaces -or- tabs:");
+  puts("Enter some words separated by spaces -or- tabs:");
+  puts("Predd Ctrl+D when you are ready to exit.");
 
-	while ((c = getchar()) != EOF) {
-		if (flag == 1 && (c == ' ' || c == '\t')) {
-			continue;
-		} else {
-			flag = 0;
-		}
-
-		if (c == ' ' || c == '\t') {
-			putchar('\n');
-			flag = 1;
-		} else {
-			putchar(c);
-		}
-	}
-	
-	return 0;
+  while ((c = getchar()) != EOF) {
+    if (flag == 1 && (c == ' ' || c == '\t')) { continue; } 
+    else { flag = 0; }
+	  
+    if (c == ' ' || c == '\t') { putchar('\n'); flag = 1; } 
+    else { putchar(c); }
+  }
+  return 0;
 }
--- Chapter_1/exercise_1.6-10/in_to_out.c
+++ Chapter_1/exercise_1.6-10/in_to_out.c
@@ -13,43 +13,29 @@
 int
 main (void) 
 {
-	int c, bc, tc, nc, space_flag;
-	c = bc = tc = nc = space_flag = 0;
+	int c, bc, tc, nc, cc, sc, sfg;
+	c = bc = tc = nc = cc = sc = sfg = 0;
 	
 	// Intro, prompting user for words
-	puts("Input some chars and press Enter");
+	puts("Input some chars and press Enter to process them.\nPress Ctrl+D when you want to exit program.");
 
 	// While we don't get a ^D (EOF), we keep going
 	while ((c = getchar()) != EOF) {
 		// If there was a space before and we see another space
 		// then increment count and move on
-		if (space_flag == 1 && c == ' ') {
-			bc++;
-			continue;
-		// Otherwise, reset the flag for continued spaces
-		} else {
-			space_flag = 0;
-		}
+		if (sfg == 1 && c == ' ') { bc++; continue; } 
+                else { sfg = 0; }
 		
 		//Char processing:
-		if (c == '\n') {  // newline
-			nc++;
-			putchar(c);
-		} else if (c == '\t') { // tab
-			tc++;
-			putchar('\\');
-			putchar('t');
-		} else if (c == '\\') { // slash
-			putchar('\\');
-			putchar('\\');
-		} else if (c == ' ') { // space
-			bc++;
-			putchar(c);
-			space_flag = 1;
-		} else { putchar(c); }
+		if (c == '\n') { nc++; putchar(c); } 
+                else if (c == '\t') { tc++; putchar('\\'); putchar('t'); } 
+                else if (c == '\\') { sc++; putchar('\\'); putchar('\\'); } 
+                else if (c == ' ') { bc++; putchar(c); sfg = 1; } 
+                else { cc++; putchar(c); }
 	}
 
 	// print out the counts
-	printf("\nBlank count = %d\nTab count = %d\nNewLine count = %d\n", bc, tc, nc);
+	printf("\nBlank Count = %d\nTab Count = %d\nNewLine Count = %d\n", bc, tc, nc);
+        printf("Slah Count = %d\nCharacter Count = %d\n", sc, cc);
 	return 0;
 }
